d893cc03eae4e6ad898f55daf3ae6d66ee123cee
Marina Wild <122946848+marinawild@users.noreply.github.com>
2023-11-20 21:23:10 -0800

    Update Ellipse.cpp

--- src/Ellipse.cpp
+++ src/Ellipse.cpp
@@ -1,1 +1,29 @@
 #include "Ellipse.h"
+// Ellipse.cpp
+
+#include "Ellipse.h"
+
+bool Ellipse::isCircle() const
+{
+    // Check if the ellipse is a circle by comparing its width and height
+    return rect.width() == rect.height();
+}
+
+void Ellipse::set_rect(const QRect& new_rect)
+{
+    // Set the rectangle for the ellipse
+    rect = new_rect;
+}
+
+void Ellipse::draw(const int translate_x, const int translate_y)
+{
+    QPainter& painter = get_qpainter();
+    
+    // Set pen and brush styles for the ellipse
+    painter.setPen(get_pen());
+    painter.setBrush(get_brush());
+    
+    // Draw the ellipse using the rectangle
+    QRect translatedRect = rect.translated(translate_x, translate_y);
+    painter.drawEllipse(translatedRect);
+}

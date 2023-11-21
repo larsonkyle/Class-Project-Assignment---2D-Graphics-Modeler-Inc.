d1008f5d6c02b7df6ce2f15c8f60ecee613f3cad
Kyle Larson <klarson22@saddleback.edu>
2023-11-21 01:38:53 -0800

    moved .txt to main project, removed extrenuous vector file, moved relevant vector file to main project and changed includes accordingly, and modified shape.h and completed most of shape.cpp implementation

--- src/Shape.cpp
+++ src/Shape.cpp
@@ -1,1 +1,71 @@
 #include "Shape.h"
+Shape::Shape(QPaintDevice* device, int setId, ShapeType sh){
+//     std::make_unique<QPainter>(device);
+    // assign paint device to qpainter??? wat
+
+    id = setId;
+    shape = sh;
+}
+
+Shape::~Shape(){
+  //unique ptr :D
+}
+
+/*****GETTERS*****/
+int Shape::getId() const{
+   return  id;
+}
+
+Shape::ShapeType Shape::get_shape() const{
+    return shape;
+}
+
+const QPen& Shape::get_pen() const{
+    return pen;
+}
+
+const QBrush& Shape::get_brush() const{
+    return brush;
+}
+//protected:
+ QPainter& Shape::get_qpainter(){
+    return *p_qpainter;
+}
+
+/*****SETTERS*******/
+void Shape::set_shape(ShapeType sh){
+    shape = sh;
+}
+
+void Shape::set_pen(Qt::GlobalColor pc, int width, Qt::PenStyle ps, Qt::PenCapStyle pcs, Qt::PenJoinStyle pjs){
+    pen.setColor(pc);
+    pen.setWidth(width);
+    pen.setStyle(ps);
+    pen.setCapStyle(pcs);
+    pen.setJoinStyle(pjs);
+}
+
+void Shape::set_pen(Qt::GlobalColor pc){
+    pen.setColor(pc);
+}
+
+void Shape::set_brush(Qt::GlobalColor bc, Qt::BrushStyle bs){
+    brush.setColor(bc);
+    brush.setStyle(bs);
+}
+
+void Shape::set_id(int setId){
+    id = setId;
+}
+/******set default style*****/
+void Shape::default_style(){
+    pen = Qt::SolidLine;
+    brush = Qt::NoBrush;
+
+    p_qpainter->setPen(pen);
+    p_qpainter->setBrush(brush);
+}
+
+void Shape::draw_rect(int width, int height){
+ //??
+}
--- src/Shape.h
+++ src/Shape.h
@@ -7,7 +7,8 @@
 #include <QPen>
 #include <QBrush>
 #include <QRect>
-#include "../vector_doubles.h"
+#include <memory>
+#include "../LoginSystem/vector_doubles.h"
 
 using namespace myStd;
 using namespace Qt;
@@ -16,18 +17,20 @@ class Shape{
 	public:
 		enum class ShapeType {NoShape, Line, Polyline, Polygon, Rectangle, Ellipse, Text};
 
-        Shape(QPaintDevice* device = nullptr, int id = -1, ShapeType shape = ShapeType::NoShape); // :;
-		virtual ~Shape() {  }
+        Shape(QPaintDevice* device = nullptr, int setId = -1, ShapeType sh = ShapeType::NoShape);
+        virtual ~Shape();
 
 
 		ShapeType get_shape() const;
 		const QPen& get_pen() const;
 		const QBrush& get_brush() const;
+        int getId() const;
 
-		void set_shape(ShapeType shape);
-		void set_pen(Qt::GlobalColor, int width, Qt::PenStyle, Qt::PenCapStyle, Qt::PenJoinStyle);
-		void set_pen(Qt::GlobalColor);
-		void set_brush(Qt::GlobalColor, Qt::BrushStyle);
+        void set_shape(ShapeType sh);
+        void set_pen(Qt::GlobalColor pc, int width, Qt::PenStyle ps, Qt::PenCapStyle pcs, Qt::PenJoinStyle pjs);
+        void set_pen(Qt::GlobalColor pc);
+        void set_brush(Qt::GlobalColor bc, Qt::BrushStyle bs);
+        void set_id(int setId);
 
 		void default_style();
 		void draw_rect(int width, int height);
@@ -38,7 +41,7 @@ class Shape{
 		QPainter& get_qpainter();
 
 	private:
-		QPainter* p_qpainter; //Maybe use Unique Pointer???
+         std::unique_ptr<QPainter> p_qpainter; //UNIQUE PTR woo
 
 		int id;
 		ShapeType shape;

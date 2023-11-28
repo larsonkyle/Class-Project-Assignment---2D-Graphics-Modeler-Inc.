e38fe86701ed17123a12519c5fc551f66b5edb5c
Marina Wild <122946848+marinawild@users.noreply.github.com>
2023-11-28 15:47:18 -0800

    Update renderArea.h slightly

--- LoginSystem/src/renderArea.h
+++ LoginSystem/src/renderArea.h
@@ -1,9 +1,10 @@
 #ifndef RENDERAREA_H
 #define RENDERAREA_H
 
+#include "vector_doubles.h"
+#include "shape.h"
 #include <QBrush>
 #include <QPen>
-#include <QPixmap>
 #include <QWidget>
 
 class RenderArea : public QWidget
@@ -11,13 +12,11 @@ class RenderArea : public QWidget
     Q_OBJECT
     
 public:
-    enum Shape { Line, Polyline, Polygon, Rect, Ellipse, Text };
-    
-    explicit RenderArea(QWidget *parent = nullptr);
-    
+    RenderArea(QWidget *parent = nullptr);
     QSize minimumSizeHint() const override;
     QSize sizeHint() const override;
-    
+    const vector<Shape*>& getShapes();
+
 public slots:
     void setShape(Shape shape);
     void setPen(const QPen &pen);
@@ -29,12 +28,13 @@ protected:
     void paintEvent(QPaintEvent *event) override;
     
 private:
+    const QImage renderArea;
+    vector<Shape*> shapes;
     Shape shape;
     QPen pen;
     QBrush brush;
     bool antialiased;
     bool transformed;
-    QPixmap pixmap;
 };
 
 #endif // RENDERAREA_H

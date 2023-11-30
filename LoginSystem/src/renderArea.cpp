977e6aec1c25d4f920f05a36de490bd3b3caf596
Marina Wild <122946848+marinawild@users.noreply.github.com>
2023-11-29 22:33:07 -0800

    Update renderArea.cpp

--- LoginSystem/src/renderArea.cpp
+++ LoginSystem/src/renderArea.cpp
@@ -1,70 +1,43 @@
+// RenderArea.cpp
+
 #include "renderArea.h"
-
 #include <QPainter>
-#include <QPainterPath>
 
-RenderArea::RenderArea(QWidget *parent)        //constructor
-    : QWidget(parent), shapeChoice(10)
+RenderArea::RenderArea(QWidget *parent)
+    : QWidget(parent)
 {
-    numberOfShapes = 0;
     setBackgroundRole(QPalette::Base);
-    setFixedSize(sizeHint());     
+    setFixedSize(sizeHint());
     setAutoFillBackground(true);
-    
-}
-void RenderArea::addShape(Shape* shape)       //adds a new shape
-{
-    shape.push_back(shape);
-    numberOfShapes++;
 }
 
-QSize RenderArea::minimumSizeHint() const   //minimum size
+void RenderArea::addShape(Shape* shape)
+{
+    shapeChoices.push_back(shape);
+    update();  // Schedule a repaint
+}
+
+QSize RenderArea::minimumSizeHint() const
 {
     return QSize(1200, 600);
 }
 
-QSize RenderArea::sizeHint() const           //size setter
+QSize RenderArea::sizeHint() const
 {
     return QSize(1200, 600);
 }
 
-void RenderArea::setShape(Shape shape)       //sets the shape
+int RenderArea::getNumOfShapes() const
 {
-    this->shape = shape;
-    update();
-}
-int RenderArea::getNumOfShapes()               //gets number of shapes in program
-{ 
-    return numberOfShapes
-}
-
-void RenderArea::setPen(const QPen &pen)
-{
-    this->pen = pen;
-    update();
-}
-
-void RenderArea::setBrush(const QBrush &brush)
-{
-    this->brush = brush;
-    update();
-}
-
-void RenderArea::setAntialiased(bool antialiased)
-{
-    this->antialiased = antialiased;
-    update();
-}
-
-void RenderArea::setTransformed(bool transformed)
-{
-    this->transformed = transformed;
-    update();
+    return shapeChoices.size();
 }
 
 void RenderArea::paintEvent(QPaintEvent *)
 {
-   QPaintDevice* shapeDevice = this;
-    for(int i = 0; i < shape.size(); i++)
-        shape[i]->draw(shapeDevice);
+    QPaintDevice *device = this;;
+
+    for (int i = 0; i < shapeChoices.size(); i++) {
+        shapeChoices[i]->draw(device);
+    }
 }
+

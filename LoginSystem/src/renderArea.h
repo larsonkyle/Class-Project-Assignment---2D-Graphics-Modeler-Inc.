bd543ec779f09c937e9974dceddac6c386e96915
Marina Wild <122946848+marinawild@users.noreply.github.com>
2023-11-29 22:32:31 -0800

    Update renderArea.h

--- LoginSystem/src/renderArea.h
+++ LoginSystem/src/renderArea.h
@@ -2,33 +2,31 @@
 #define RENDERAREA_H
 
 #include "vector_doubles.h"
-#include "shape.h"
+#include "Shape.h"
 #include <QBrush>
+#include <QPaintEvent>
 #include <QPen>
 #include <QWidget>
+#include <QFile>
+#include <QMessageBox>
 
 class RenderArea : public QWidget
-{   
+{
+    Q_OBJECT
 public:
     RenderArea(QWidget *parent = nullptr);
     QSize minimumSizeHint() const override;
     QSize sizeHint() const override;
-    const vector<Shape*>& getShape();
-    void addShape(Shape* shape)
-    int getSize();
-    int getNumOfShapes();
-    void paintEvent(QPaintEvent *event) override;    void setShape(Shape shape);
-    
-    void setPen(const QPen &pen);
-    void setBrush(const QBrush &brush);
-    void setAntialiased(bool antialiased);
-    void setTransformed(bool transformed);
+    const vector<Shape*>& getShape() const;
+    void addShape(Shape* shapeAdd);
+    int getSize() const;
+    int getNumOfShapes() const;
     void paintEvent(QPaintEvent *event) override;
-    
+
 private:
-    const QImage renderArea;
+    QImage renderArea;
     int numberOfShapes;
-    vector<Shape*> shapeChoice;
+    vector<Shape*> shapeChoices;
 };
 
 #endif // RENDERAREA_H

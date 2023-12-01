ac9ec5b9bbad4e64c4e01adc8be07ec6a8215fa4
Marina Wild <122946848+marinawild@users.noreply.github.com>
2023-12-01 09:37:32 -0800

    Update renderArea.h

--- LoginSystem/src/renderArea.h
+++ LoginSystem/src/renderArea.h
@@ -1,18 +1,21 @@
 #ifndef RENDERAREA_H
 #define RENDERAREA_H
 
-#include "vector_doubles.h"
 #include "Shape.h"
+#include "vector_doubles.h"
 #include <QBrush>
-#include <QPaintEvent>
 #include <QPen>
 #include <QWidget>
-#include <QFile>
-#include <QMessageBox>
+#include <QImage>
+#include <QMenu>
+#include <QContextMenuEvent>
+
+class Shape;
 
 class RenderArea : public QWidget
 {
     Q_OBJECT
+
 public:
     RenderArea(QWidget *parent = nullptr);
     QSize minimumSizeHint() const override;
@@ -21,12 +24,17 @@ public:
     void addShape(Shape* shapeAdd);
     int getSize() const;
     int getNumOfShapes() const;
+    //QMenu* getShapeMenu() const;
+
+protected:
     void paintEvent(QPaintEvent *event) override;
+    //void contextMenuEvent(QContextMenuEvent *event) override;
 
 private:
     QImage renderArea;
     int numberOfShapes;
     vector<Shape*> shapeChoices;
+    //QMenu* shapeMenu;
 };
 
 #endif // RENDERAREA_H
